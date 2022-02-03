7756c9fc4eb1da2380b0c48baa1f69d8363d5a16
Antoine Droyer <79838275+Bigoou@users.noreply.github.com>
2022-02-03 11:15:45 +0100

    Update api-post-render_backup.php

--- api-post-render_backup.php
+++ api-post-render_backup.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header('content-type:application/json');      
 header('Access-Control-Allow-Headers: Content-Type');  
       
-      $db = new PDO('mysql:host=db5005161444.hosting-data.io;dbname=dbs4318125', 'dbu1522474', 'lesoussol06092021', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+      $db = new PDO('mysql:host=', '', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
       $req = "SELECT * FROM post ORDER BY id_post desc";
       $stmt=$db->query($req);
       $posts=$stmt->fetchAll(PDO::FETCH_ASSOC);
